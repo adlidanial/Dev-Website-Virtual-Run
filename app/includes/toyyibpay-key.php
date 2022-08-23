69b002117cfacbec494fe6eff2e3079a886eec47
adlidanial <56016225+adlidanial@users.noreply.github.com>
2022-08-23 15:50:44 +0800

    update toyyibpay-key.php

--- app/includes/toyyibpay-key.php
+++ app/includes/toyyibpay-key.php
@@ -1,13 +1,13 @@
 <?php
     // Production ToyyibPay - Sesi 1
     
-    $secretkey = 'zd71inea-c2pu-efn5-1ims-h6om7fz6ald6';
+    $secretkey = 'your_secret_key';
     $categorycode = 'bz86c1h8';
     
     
     // Development ToyyibPay
     /*
-    $secretkey = 'kdewc25e-hqpd-8iqy-ury8-1bzezqlvyzkf';
+    $secretkey = 'your_secret_key';
     $categorycode = '6rfubpjc';
     */
     
